f4c66bf26b464bfd0e702e21cd10f80972c3511e
Martin Kamp Jensen <martin.kamp.jensen@gmail.com>
2017-10-15 15:04:51 +0200

    Move com.github.mkjensen.tv.ui to com.github.mkjensen.tv.ondemand

--- app/src/main/java/com/github/mkjensen/tv/ondemand/AboutFragment.java
+++ app/src/main/java/com/github/mkjensen/tv/ondemand/AboutFragment.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.github.mkjensen.tv.ui;
+package com.github.mkjensen.tv.ondemand;
 
 import android.app.Activity;
 import android.content.Context;
